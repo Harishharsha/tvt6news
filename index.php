3e50392fd3de143bca6c3b30e46448e3719791f8
Annikapati Harish <harish2012.2012@gmail.com>
2019-09-03 15:45:17 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,419 @@
+<!DOCTYPE html>
+<html lang="en">
+       <head>
+	      <meta charset="utf-8">
+		  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+              <title>AMPLE-2K19</title>
+		<meta name="keywords" content="AMPLE2k19,AMPLE,SVCEAMPLE,AMPLE SVCE,2019 AMPLE,AMPLE-2K19,AMPLE-2019,AMPLE AMPLE,AMPLE SVCE,AMPLE SVEW,SVCOLLEGES AMPLE, AMPLE SVCOLLEGES ">
+		<meta name="description" content=" AMPLE 2K18 | SVCOLLEGES[SVCE|SVEW] | TIRUPATI. ">
+		<meta name="author" content="AMPLE 2K18 | SVCOLLEGES[SVCE|SVEW] | TIRUPATI">
+			   
+      <link rel="stylesheet" href="css/css.css" />
+	  <link rel="stylesheet" href="css/slide.css">
+	<link rel="stylesheet" media="(max-width:640px)" href="css/mobile.css">
+	    <link rel="stylesheet" href="css/responsee.css">
+		 
+		 
+    
+      <!-- CUSTOM STYLE --> <link rel="stylesheet" href="css/template-style.css">
+	  
+<script type="application/ld+json">
+{
+  "@context": "http://www.svcolleges.edu.in/AMPLE2019/",
+  "@type": "SVCOLLEGE TECHNICAL FEST",
+  "url": "http://www.svcolleges.edu.in/AMPLE2019/",
+  "contactPoint": [{
+    "@type": "svcolleges ,opposite LIC Training center,Karakambadi road,Tirupati,517 501.",
+    "telephone": " 9642942897,9885492034 ",
+    "contactType": "AMPLE 2019 - SV COLLEGE TECHNICAL FEST."
+  }]
+}
+</script>
+
+
+	
+<!--<script src="//fast.eager.io/ftr1cej8MG.js"></script>
+      <script src="//fast.eager.io/ftr1cej8MG.js"></script>	-->
+      <script type="text/javascript" src="js/jquery-1.8.3.min.js"></script>
+      <script type="text/javascript" src="js/jquery-ui.min.js"></script>    
+      <script type="text/javascript" src="js/modernizr.js"></script>
+      <script type="text/javascript" src="js/responsee.js"></script>
+	
+
+
+
+
+    
+	</head>
+    
+<body>
+
+
+<div id="top">
+	<div id="topleft">
+		<strong>Connect Us @ &nbsp;</strong>
+			 <a href="https://www.facebook.com/SVCETPT/"><strong>FACEBOOK </strong></a>
+	</div>
+	
+	<div id="toptext">
+		 <strong>Call US :: </strong> &nbsp;
+		 <strong> 8886644961 | 9885492034 | 9642942897  </strong>. 
+	 </div>
+</div>
+
+
+
+
+
+
+
+	
+	 <header>
+            <nav>
+               <div class="line">
+                  <div class="s-12 l-2">
+                   <div id="logotext1"><strong>AMPLE 2K19</strong><br></div> 
+                  </div>
+                  <div class="top-nav s-12 l-10 right">
+                     <p class="nav-text">MENU</p>
+                     <ul class="right">
+                        <li><a href="index.php"><strong>Home</strong></a></li>
+                        <li><a href="aboutus.html"><strong>About us</strong></a></li>
+						<li><a href="schedule.html"><strong>Schedule</strong></a></li>
+                        <li><a ><strong>Workshop's</strong></a>
+                           <ul>
+                              <li><a href="ANSYS.html"><strong>1.ANSYS </strong></a></li>
+							  <li><a href="ETABS.html"><strong>2.ETABS </strong></a></li>
+							  <li><a href="MATLAB.html"><strong>3.MATLAB</strong></a></li>
+							  <li><a href="QUADCOPTER.html"><strong>4.QUADCOPTER </strong></a></li>
+                            <li><a href="CYBERSECURITY.html"><strong>5.CYBER SECURITY</strong></a></li>
+						   </ul>
+					   </li>
+					      <!--<li><a ><strong>EVENTS</strong></a>
+                           <ul>
+                              <li><a href="event1.html"><strong>1. CSE</strong></a></li>
+							  <li><a href="event2.html"><strong>2. ECE </strong></a></li>
+							  <li><a href="event3.html"><strong>3. EEE</strong></a></li>
+							  <li><a href="event4.html"><strong>4. MECH </strong></a></li>
+                              <li><a href="event5.html"><strong>5. CIVIL</strong></a></li>
+							  <li><a href="event6.html"><strong>6. IT </strong></a></li>
+						  </ul>
+					   </li>-->
+                       <li><a href="contact.html"><strong>Contact Us</strong></a></li>
+					  
+                     </ul>
+                  </div>
+               </div>
+            </nav>
+         </header>
+
+    
+
+
+<div id="slide">
+
+	<div class="slideshow-container">
+
+		<div class="mySlides fade"><img src="img/m1.jpg" style="width:100%">
+		<span class="dot"></span></div>
+
+		<div class="mySlides fade"><img src="img/m2.jpg" style="width:100%">
+		<span class="dot"></span></div>
+
+
+		<div class="mySlides fade"><img src="img/m3.jpg" style="width:100%">
+		<span class="dot"></span></div>
+
+
+		<div class="mySlides fade"><img src="img/m4.jpg" style="width:100%">
+		<span class="dot"></span></div>
+
+
+		<div class="mySlides fade"><img src="img/m5.jpg" style="width:100%">
+		<span class="dot"></span></div>
+
+
+		<div class="mySlides fade"><img src="img/m6.jpg" style="width:100%">
+		<span class="dot"></span></div>
+
+
+		<div class="mySlides fade"><img src="img/m7.jpg" style="width:100%">
+		<span class="dot"></span></div>
+
+		<div class="mySlides fade"><img src="img/m8.jpg" style="width:100%">
+		<span class="dot"></span></div>
+	</div>
+	
+</div>
+
+
+<div id="jmain">
+<div id="jm1">
+<div id="pjm1"><img src="img/logo.png" alt="lo" class="responsive" width="100%" height="100%"></div>
+<div id="docn">
+	<p align="left"><strong>SV COLLEGE OF ENGINEERING </strong></p>
+	<p align="center" style="font-size:18px" align="left">[ SVCE | SVEW ] </p></p>
+</div>
+</div>
+	
+
+<div id="jm2">
+<div id="hsptl">
+<strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SV COLLEGES-</strong> AMPLE 2K19 is synonymous to one of the biggest vibrant techno-cultural fests with throngs of active participants. It is initiated with an aim of providing opportunities to the student community to bring out the immense talent in the competitions modeled to test the technical acumen, programming skills, flair for innovation, and cultural abilities of the participants. .<br><br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;It is an elite congregation of students from renowned institutions from all over the country. The fest provides a plethora of events for engineering students to prove their caliber in their fields of interest. It is a unique learning opportunity for budding engineers, where they interact with their seniors and peer groups. It provides the participants ample opportunities for exchanging their views and sharing their knowledge with wide spectrum of students. The quality of participation and delegation itself promotes an ambiance for knowledge and exchange of ideas not be restricted to one notion. An engineer grows in knowledge, skills and experience after attending AMPLE. </div>
+</div>
+</div>	
+
+
+
+
+<div id="event">
+ <div id="eventtext"><strong>AMPLE 2K19- Workshops</strong><br></div>
+ <br> 
+ <div id="eventtext1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>AMPLE-2019-A National Level Technical Symposium</strong> hosts five workshops from various departments [CSE/EEE/ECE/MECH/IT] on upcoming technologies & quite trending topics. Ample  provides a collective environment to participate and gain exposure one among such workshops by enrolling your details.</div> 
+ </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+<div id="mmain">
+
+<div id="t1card">
+	<div id="t1card1"><img src="img/cyber.jpg" alt="ws1" class="responsive" width="100%" height="100%"></div>
+		<div id="t1card3">
+			<div align="center" id="hsptl1"><strong>
+					<a href="cybersecurity.html">ANSYS</strong></a>
+			</div>
+		</div>
+</div>
+
+
+
+
+<div id="t2card">
+	<div id="t2card1"><img src="img/cyber.jpg" alt="ws1" class="responsive" width="100%" height="100%"></div>
+		<div id="t2card3">
+			<div align="center" id="hsptl1"><strong>
+					<a href="cybersecurity.html">ETABS</strong></a>
+			</div>
+		</div>
+</div>
+
+<div id="t3card">
+	<div id="t3card1"><img src="img/cyber.jpg" alt="ws1" class="responsive" width="100%" height="100%"></div>
+		<div id="t3card3">
+			<div align="center" id="hsptl1"><strong>
+					<a href="cybersecurity.html">MAT LAB</strong></a>
+			</div>
+		</div>
+</div>
+
+<div id="t4card">
+	<div id="t4card1"><img src="img/cyber.jpg" alt="ws1" class="responsive" width="100%" height="100%"></div>
+		<div id="t4card3">
+			<div align="center" id="hsptl1"><strong>
+					<a href="cybersecurity.html">QUADCOPTER</strong></a>
+			</div>
+		</div>
+</div>
+
+<div id="t5card">
+	<div id="t5card1"><img src="img/cyber.jpg" alt="ws1" class="responsive" width="100%" height="100%"></div>
+		<div id="t5card3">
+			<div align="center" id="hsptl1"><strong>
+					<a href="cybersecurity.html">CYBER SECURITY</strong></a>
+			</div>
+		</div>
+</div>
+
+
+</div>
+
+
+
+
+
+<div id="aevent">
+
+
+ <div id="aeventtext"><strong>AMPLE 2K19- EVENT'S</strong><br></div>
+ <br> 
+ <div id="aeventtext1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>AMPLE-2019-A National Level Technical Symposium</strong>  hosts several events that will let you reach newer heights of competition and competence. We invite engineering students of all colleges to take part in AMPLE-2019 and win prizes. </div> 
+  <div id="aeventtext2"><strong>&quot;&nbsp;AMPLE-2018 Witness the grandeur of AMPLE’19, it’s the year for new experiences.&nbsp;&quot;</strong>
+ </div>
+ 
+ 
+ <div id="at1card">
+	<div id="at1card1"><img src="img/cod.jpg" alt="ws1" class="responsive" width="100%" height="100%"></div>
+		<div id="at1card3">
+			<div align="center" id="ahsptl1"><strong>
+					<a href="cybersecurity.html">EVENT 1</strong></a>
+			</div>
+		</div>
+</div>
+ 
+ 
+  <div id="at2card">
+	<div id="at2card1"><img src="img/cod.jpg" alt="ws1" class="responsive" width="100%" height="100%"></div>
+		<div id="at2card3">
+			<div align="center" id="ahsptl1"><strong>
+					<a href="cybersecurity.html">EVENT 2</strong></a>
+			</div>
+		</div>
+</div>
+ 
+   <div id="at3card">
+	<div id="at3card1"><img src="img/cod.jpg" alt="ws1" class="responsive" width="100%" height="100%"></div>
+		<div id="at3card3">
+			<div align="center" id="ahsptl1"><strong>
+					<a href="cybersecurity.html">EVENT 3</strong></a>
+			</div>
+		</div>
+</div>
+ 
+    <div id="at4card">
+	<div id="at4card1"><img src="img/cod.jpg" alt="ws1" class="responsive" width="100%" height="100%"></div>
+		<div id="at4card3">
+			<div align="center" id="ahsptl1"><strong>
+					<a href="cybersecurity.html">EVENT 4</strong></a>
+			</div>
+		</div>
+</div>
+
+    <div id="at5card">
+	<div id="at5card1"><img src="img/cod.jpg" alt="ws1" class="responsive" width="100%" height="100%"></div>
+		<div id="at5card3">
+			<div align="center" id="ahsptl1"><strong>
+					<a href="cybersecurity.html">EVENT 5</strong></a>
+			</div>
+		</div>
+</div>
+ 
+ 
+ 
+ 
+ 
+ 
+  
+ <div id="aat1card">
+	<div id="aat1card1"><img src="img/cod.jpg" alt="ws1" class="responsive" width="100%" height="100%"></div>
+		<div id="aat1card3">
+			<div align="center" id="ahsptl1"><strong>
+					<a href="cybersecurity.html">EVENT 6</strong></a>
+			</div>
+		</div>
+</div>
+ 
+ 
+  <div id="aat2card">
+	<div id="aat2card1"><img src="img/cod.jpg" alt="ws1" class="responsive" width="100%" height="100%"></div>
+		<div id="aat2card3">
+			<div align="center" id="ahsptl1"><strong>
+					<a href="cybersecurity.html">EVENT 7</strong></a>
+			</div>
+		</div>
+</div>
+ 
+   <div id="aat3card">
+	<div id="aat3card1"><img src="img/cod.jpg" alt="ws1" class="responsive" width="100%" height="100%"></div>
+		<div id="aat3card3">
+			<div align="center" id="ahsptl1"><strong>
+					<a href="cybersecurity.html">EVENT 8</strong></a>
+			</div>
+		</div>
+</div>
+ 
+    <div id="aat4card">
+	<div id="aat4card1"><img src="img/cod.jpg" alt="ws1" class="responsive" width="100%" height="100%"></div>
+		<div id="aat4card3">
+			<div align="center" id="ahsptl1"><strong>
+					<a href="cybersecurity.html">EVENT 9</strong></a>
+			</div>
+		</div>
+</div>
+
+    <div id="aat5card">
+	<div id="aat5card1"><img src="img/cod.jpg" alt="ws1" class="responsive" width="100%" height="100%"></div>
+		<div id="aat5card3">
+			<div align="center" id="ahsptl1"><strong>
+					<a href="cybersecurity.html">EVENT 10</strong></a>
+			</div>
+		</div>
+</div>
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div id="end">
+	<div id="endl">Copyright &#169;&nbsp;2016, &nbsp;&nbsp;SV COLLEGES | SVCE | SVEW | TIRUPATI. &nbsp;&nbsp;&nbsp;&nbsp; Svcolleges,Opposite LIC Training center,Karakambadi road,Tirupati,517 501.
+	</div>
+</div>
+
+<script>
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+</script>
+
+
+
+</body>
+</html>
