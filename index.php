c144a8e2f8d4d85ccf123b614b416ff2a3800ce7
Annikapati Harish <harish2012.2012@gmail.com>
2019-09-04 21:00:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,33 +3,20 @@
        <head>
 	      <meta charset="utf-8">
 		  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-              <title>AMPLE-2K19</title>
-		<meta name="keywords" content="AMPLE2k19,AMPLE,SVCEAMPLE,AMPLE SVCE,2019 AMPLE,AMPLE-2K19,AMPLE-2019,AMPLE AMPLE,AMPLE SVCE,AMPLE SVEW,SVCOLLEGES AMPLE, AMPLE SVCOLLEGES ">
-		<meta name="description" content=" AMPLE 2K18 | SVCOLLEGES[SVCE|SVEW] | TIRUPATI. ">
-		<meta name="author" content="AMPLE 2K18 | SVCOLLEGES[SVCE|SVEW] | TIRUPATI">
+              <title>T6 TV NEWS</title>
+		<meta name="keywords" content="">
+		<meta name="description" content="  ">
+		<meta name="author" content="">
 			   
       <link rel="stylesheet" href="css/css.css" />
 	  <link rel="stylesheet" href="css/slide.css">
-	<link rel="stylesheet" media="(max-width:640px)" href="css/mobile.css">
+	
 	    <link rel="stylesheet" href="css/responsee.css">
 		 
 		 
     
       <!-- CUSTOM STYLE --> <link rel="stylesheet" href="css/template-style.css">
-	  
-<script type="application/ld+json">
-{
-  "@context": "http://www.svcolleges.edu.in/AMPLE2019/",
-  "@type": "SVCOLLEGE TECHNICAL FEST",
-  "url": "http://www.svcolleges.edu.in/AMPLE2019/",
-  "contactPoint": [{
-    "@type": "svcolleges ,opposite LIC Training center,Karakambadi road,Tirupati,517 501.",
-    "telephone": " 9642942897,9885492034 ",
-    "contactType": "AMPLE 2019 - SV COLLEGE TECHNICAL FEST."
-  }]
-}
-</script>
-
+	
 
 	
 <!--<script src="//fast.eager.io/ftr1cej8MG.js"></script>
@@ -52,12 +39,15 @@
 <div id="top">
 	<div id="topleft">
 		<strong>Connect Us @ &nbsp;</strong>
-			 <a href="https://www.facebook.com/SVCETPT/"><strong>FACEBOOK </strong></a>
+			 <a href="#"><strong>FACEBOOK </strong></a>
 	</div>
 	
 	<div id="toptext">
-		 <strong>Call US :: </strong> &nbsp;
-		 <strong> 8886644961 | 9885492034 | 9642942897  </strong>. 
+		 <strong>Contact US :: </strong> &nbsp;
+		 <strong>24-264/1, NTR Nagar, Jagathgirigutta, Medchal Dist-55   </strong>&nbsp;&nbsp;&nbsp;&nbsp;
+		 
+		 <strong>Help Line:: </strong> &nbsp;
+		 <strong>9160044744.</strong>
 	 </div>
 </div>
 
@@ -72,34 +62,24 @@
             <nav>
                <div class="line">
                   <div class="s-12 l-2">
-                   <div id="logotext1"><strong>AMPLE 2K19</strong><br></div> 
+					  <div id="img1"><img src="img/logo.png" alt="lo" class="responsive" width="100%" height="100%"></div>
+                   <div id="logotext1"><strong>T6 TV NEWS</strong><br></div> 
                   </div>
                   <div class="top-nav s-12 l-10 right">
                      <p class="nav-text">MENU</p>
                      <ul class="right">
-                        <li><a href="index.php"><strong>Home</strong></a></li>
-                        <li><a href="aboutus.html"><strong>About us</strong></a></li>
-						<li><a href="schedule.html"><strong>Schedule</strong></a></li>
-                        <li><a ><strong>Workshop's</strong></a>
+                        <li><a href="index.html"><strong>Home</strong></a></li>
+                        <li><a href="index1.html"><strong>About us</strong></a></li>
+						
+                        <li><a ><strong>Telugu States</strong></a>
                            <ul>
-                              <li><a href="ANSYS.html"><strong>1.ANSYS </strong></a></li>
-							  <li><a href="ETABS.html"><strong>2.ETABS </strong></a></li>
-							  <li><a href="MATLAB.html"><strong>3.MATLAB</strong></a></li>
-							  <li><a href="QUADCOPTER.html"><strong>4.QUADCOPTER </strong></a></li>
-                            <li><a href="CYBERSECURITY.html"><strong>5.CYBER SECURITY</strong></a></li>
+                              <li><a href="#"><strong>Andhra Pradesh </strong></a></li>
+							  <li><a href="#"><strong>Telangana </strong></a></li>
+							  
 						   </ul>
 					   </li>
-					      <!--<li><a ><strong>EVENTS</strong></a>
-                           <ul>
-                              <li><a href="event1.html"><strong>1. CSE</strong></a></li>
-							  <li><a href="event2.html"><strong>2. ECE </strong></a></li>
-							  <li><a href="event3.html"><strong>3. EEE</strong></a></li>
-							  <li><a href="event4.html"><strong>4. MECH </strong></a></li>
-                              <li><a href="event5.html"><strong>5. CIVIL</strong></a></li>
-							  <li><a href="event6.html"><strong>6. IT </strong></a></li>
-						  </ul>
-					   </li>-->
-                       <li><a href="contact.html"><strong>Contact Us</strong></a></li>
+					      
+                       <li><a href="#"><strong>Contact Us</strong></a></li>
 					  
                      </ul>
                   </div>
@@ -117,31 +97,14 @@
 		<div class="mySlides fade"><img src="img/m1.jpg" style="width:100%">
 		<span class="dot"></span></div>
 
-		<div class="mySlides fade"><img src="img/m2.jpg" style="width:100%">
+
+		<div class="mySlides fade"><img src="img/m1.jpg" style="width:100%">
 		<span class="dot"></span></div>
 
 
-		<div class="mySlides fade"><img src="img/m3.jpg" style="width:100%">
+		<div class="mySlides fade"><img src="img/m1.jpg" style="width:100%">
 		<span class="dot"></span></div>
 
-
-		<div class="mySlides fade"><img src="img/m4.jpg" style="width:100%">
-		<span class="dot"></span></div>
-
-
-		<div class="mySlides fade"><img src="img/m5.jpg" style="width:100%">
-		<span class="dot"></span></div>
-
-
-		<div class="mySlides fade"><img src="img/m6.jpg" style="width:100%">
-		<span class="dot"></span></div>
-
-
-		<div class="mySlides fade"><img src="img/m7.jpg" style="width:100%">
-		<span class="dot"></span></div>
-
-		<div class="mySlides fade"><img src="img/m8.jpg" style="width:100%">
-		<span class="dot"></span></div>
 	</div>
 	
 </div>
@@ -151,22 +114,21 @@
 <div id="jm1">
 <div id="pjm1"><img src="img/logo.png" alt="lo" class="responsive" width="100%" height="100%"></div>
 <div id="docn">
-	<p align="left"><strong>SV COLLEGE OF ENGINEERING </strong></p>
-	<p align="center" style="font-size:18px" align="left">[ SVCE | SVEW ] </p></p>
+	<p align="left"><strong>T6 TV NEWS </strong></p>
+	<p align="center" style="font-size:18px" align="left">[ AP | TS ] </p></p>
 </div>
 </div>
 	
 
 <div id="jm2">
 <div id="hsptl">
-<strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SV COLLEGES-</strong> AMPLE 2K19 is synonymous to one of the biggest vibrant techno-cultural fests with throngs of active participants. It is initiated with an aim of providing opportunities to the student community to bring out the immense talent in the competitions modeled to test the technical acumen, programming skills, flair for innovation, and cultural abilities of the participants. .<br><br>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;It is an elite congregation of students from renowned institutions from all over the country. The fest provides a plethora of events for engineering students to prove their caliber in their fields of interest. It is a unique learning opportunity for budding engineers, where they interact with their seniors and peer groups. It provides the participants ample opportunities for exchanging their views and sharing their knowledge with wide spectrum of students. The quality of participation and delegation itself promotes an ambiance for knowledge and exchange of ideas not be restricted to one notion. An engineer grows in knowledge, skills and experience after attending AMPLE. </div>
+<strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;T6 TV NEWS-</strong> Introduction about the T6 TV NEWS</div>
 </div>
 </div>	
 
 
 
-
+<!--
 <div id="event">
  <div id="eventtext"><strong>AMPLE 2K19- Workshops</strong><br></div>
  <br> 
@@ -373,7 +335,7 @@
 
 
 
-
+-->
 
 
 
@@ -387,7 +349,7 @@
 
 
 <div id="end">
-	<div id="endl">Copyright &#169;&nbsp;2016, &nbsp;&nbsp;SV COLLEGES | SVCE | SVEW | TIRUPATI. &nbsp;&nbsp;&nbsp;&nbsp; Svcolleges,Opposite LIC Training center,Karakambadi road,Tirupati,517 501.
+	<div id="endl">Copyright &#169;&nbsp;2019, &nbsp;&nbsp;T6 TV NEWS | AP | TS | Hyderabad. &nbsp;&nbsp;&nbsp;&nbsp; 
 	</div>
 </div>
 
