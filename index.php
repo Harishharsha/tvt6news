b6ac29c24cbca5bcef5e6684cc1877827194d140
Annikapati Harish <harish2012.2012@gmail.com>
2019-11-01 19:49:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 		<meta name="author" content="">
 			   
       <link rel="stylesheet" href="css/css.css" />
+      <link rel="stylesheet" href="css/mobile.css" />
 	  <link rel="stylesheet" href="css/slide.css">
 	
 	    <link rel="stylesheet" href="css/responsee.css">
@@ -69,11 +70,11 @@
                      <p class="nav-text">MENU</p>
                      <ul class="right">
                         <li><a href="index.html"><strong>Home</strong></a></li>
-                        <li><a href="index1.html"><strong>About us</strong></a></li>
+                        <li><a href="#"><strong>About us</strong></a></li>
 						
                         <li><a ><strong>Telugu States</strong></a>
                            <ul>
-                              <li><a href="ap.html"><strong>Andhra Pradesh </strong></a></li>
+                              <li><a href="#"><strong>Andhra Pradesh </strong></a></li>
 							  <li><a href="#"><strong>Telangana </strong></a></li>
 							  
 						   </ul>
@@ -122,230 +123,14 @@
 
 <div id="jm2">
 <div id="hsptl">
-<strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;T6 TV NEWS-</strong> Introduction about the T6 TV NEWS</div>
+<strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;T6 TV NEWS-</strong> Introduction about the T6 TV NEWS
+<strong class='md'><br/><br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Kompally Satyanarayana Guptha</strong>
+<strong class='md1'><br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Managing Director</strong>
+<strong class='md2'><br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Regd.No: 543/2019</strong>
+<strong class='md3'><br/><br/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;9160044744</strong>
+</div>
 </div>
 </div>	
-
-
-
-<!--
-<div id="event">
- <div id="eventtext"><strong>AMPLE 2K19- Workshops</strong><br></div>
- <br> 
- <div id="eventtext1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>AMPLE-2019-A National Level Technical Symposium</strong> hosts five workshops from various departments [CSE/EEE/ECE/MECH/IT] on upcoming technologies & quite trending topics. Ample  provides a collective environment to participate and gain exposure one among such workshops by enrolling your details.</div> 
- </div>
-</div>
-
-
-
-
-
-
-
-
-
-
-<div id="mmain">
-
-<div id="t1card">
-	<div id="t1card1"><img src="img/cyber.jpg" alt="ws1" class="responsive" width="100%" height="100%"></div>
-		<div id="t1card3">
-			<div align="center" id="hsptl1"><strong>
-					<a href="cybersecurity.html">ANSYS</strong></a>
-			</div>
-		</div>
-</div>
-
-
-
-
-<div id="t2card">
-	<div id="t2card1"><img src="img/cyber.jpg" alt="ws1" class="responsive" width="100%" height="100%"></div>
-		<div id="t2card3">
-			<div align="center" id="hsptl1"><strong>
-					<a href="cybersecurity.html">ETABS</strong></a>
-			</div>
-		</div>
-</div>
-
-<div id="t3card">
-	<div id="t3card1"><img src="img/cyber.jpg" alt="ws1" class="responsive" width="100%" height="100%"></div>
-		<div id="t3card3">
-			<div align="center" id="hsptl1"><strong>
-					<a href="cybersecurity.html">MAT LAB</strong></a>
-			</div>
-		</div>
-</div>
-
-<div id="t4card">
-	<div id="t4card1"><img src="img/cyber.jpg" alt="ws1" class="responsive" width="100%" height="100%"></div>
-		<div id="t4card3">
-			<div align="center" id="hsptl1"><strong>
-					<a href="cybersecurity.html">QUADCOPTER</strong></a>
-			</div>
-		</div>
-</div>
-
-<div id="t5card">
-	<div id="t5card1"><img src="img/cyber.jpg" alt="ws1" class="responsive" width="100%" height="100%"></div>
-		<div id="t5card3">
-			<div align="center" id="hsptl1"><strong>
-					<a href="cybersecurity.html">CYBER SECURITY</strong></a>
-			</div>
-		</div>
-</div>
-
-
-</div>
-
-
-
-
-
-<div id="aevent">
-
-
- <div id="aeventtext"><strong>AMPLE 2K19- EVENT'S</strong><br></div>
- <br> 
- <div id="aeventtext1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<strong>AMPLE-2019-A National Level Technical Symposium</strong>  hosts several events that will let you reach newer heights of competition and competence. We invite engineering students of all colleges to take part in AMPLE-2019 and win prizes. </div> 
-  <div id="aeventtext2"><strong>&quot;&nbsp;AMPLE-2018 Witness the grandeur of AMPLE’19, it’s the year for new experiences.&nbsp;&quot;</strong>
- </div>
- 
- 
- <div id="at1card">
-	<div id="at1card1"><img src="img/cod.jpg" alt="ws1" class="responsive" width="100%" height="100%"></div>
-		<div id="at1card3">
-			<div align="center" id="ahsptl1"><strong>
-					<a href="cybersecurity.html">EVENT 1</strong></a>
-			</div>
-		</div>
-</div>
- 
- 
-  <div id="at2card">
-	<div id="at2card1"><img src="img/cod.jpg" alt="ws1" class="responsive" width="100%" height="100%"></div>
-		<div id="at2card3">
-			<div align="center" id="ahsptl1"><strong>
-					<a href="cybersecurity.html">EVENT 2</strong></a>
-			</div>
-		</div>
-</div>
- 
-   <div id="at3card">
-	<div id="at3card1"><img src="img/cod.jpg" alt="ws1" class="responsive" width="100%" height="100%"></div>
-		<div id="at3card3">
-			<div align="center" id="ahsptl1"><strong>
-					<a href="cybersecurity.html">EVENT 3</strong></a>
-			</div>
-		</div>
-</div>
- 
-    <div id="at4card">
-	<div id="at4card1"><img src="img/cod.jpg" alt="ws1" class="responsive" width="100%" height="100%"></div>
-		<div id="at4card3">
-			<div align="center" id="ahsptl1"><strong>
-					<a href="cybersecurity.html">EVENT 4</strong></a>
-			</div>
-		</div>
-</div>
-
-    <div id="at5card">
-	<div id="at5card1"><img src="img/cod.jpg" alt="ws1" class="responsive" width="100%" height="100%"></div>
-		<div id="at5card3">
-			<div align="center" id="ahsptl1"><strong>
-					<a href="cybersecurity.html">EVENT 5</strong></a>
-			</div>
-		</div>
-</div>
- 
- 
- 
- 
- 
- 
-  
- <div id="aat1card">
-	<div id="aat1card1"><img src="img/cod.jpg" alt="ws1" class="responsive" width="100%" height="100%"></div>
-		<div id="aat1card3">
-			<div align="center" id="ahsptl1"><strong>
-					<a href="cybersecurity.html">EVENT 6</strong></a>
-			</div>
-		</div>
-</div>
- 
- 
-  <div id="aat2card">
-	<div id="aat2card1"><img src="img/cod.jpg" alt="ws1" class="responsive" width="100%" height="100%"></div>
-		<div id="aat2card3">
-			<div align="center" id="ahsptl1"><strong>
-					<a href="cybersecurity.html">EVENT 7</strong></a>
-			</div>
-		</div>
-</div>
- 
-   <div id="aat3card">
-	<div id="aat3card1"><img src="img/cod.jpg" alt="ws1" class="responsive" width="100%" height="100%"></div>
-		<div id="aat3card3">
-			<div align="center" id="ahsptl1"><strong>
-					<a href="cybersecurity.html">EVENT 8</strong></a>
-			</div>
-		</div>
-</div>
- 
-    <div id="aat4card">
-	<div id="aat4card1"><img src="img/cod.jpg" alt="ws1" class="responsive" width="100%" height="100%"></div>
-		<div id="aat4card3">
-			<div align="center" id="ahsptl1"><strong>
-					<a href="cybersecurity.html">EVENT 9</strong></a>
-			</div>
-		</div>
-</div>
-
-    <div id="aat5card">
-	<div id="aat5card1"><img src="img/cod.jpg" alt="ws1" class="responsive" width="100%" height="100%"></div>
-		<div id="aat5card3">
-			<div align="center" id="ahsptl1"><strong>
-					<a href="cybersecurity.html">EVENT 10</strong></a>
-			</div>
-		</div>
-</div>
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
-</div>
-
-
-
-
-
-
-
-
--->
-
-
-
-
-
-
-
-
-
-
 
 
 <div id="end">
